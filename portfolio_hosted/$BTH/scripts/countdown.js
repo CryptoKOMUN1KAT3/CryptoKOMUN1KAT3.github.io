676c4063f258ee2e0b5612ab27e20854c832efda
KOMUN1KAT3 <143229995+CryptoKOMUN1KAT3@users.noreply.github.com>
2024-02-21 22:41:41 +0000

    Add files via upload

---
+++ portfolio_hosted/$BTH/scripts/countdown.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const endTime = new Date("April 17, 2024 22:03:24").getTime();
+  const timerElement = document.getElementById("timer");
+
+  const updateCountdown = () => {
+    const now = new Date().getTime();
+    const distance = endTime - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Calculate milliseconds as the remainder of seconds divided, scaled to display as a 3-digit number
+    const milliseconds = Math.floor((distance % 1000) / 100);
+
+    timerElement.innerHTML =
+      "BTC Halving In: " +
+      days +
+      "d " +
+      hours +
+      "h " +
+      minutes +
+      "m " +
+      seconds +
+      "s " +
+      milliseconds +
+      "ms";
+
+    if (distance < 0) {
+      clearInterval(timerInterval);
+      timerElement.innerHTML = "The halving has occurred!";
+    }
+  };
+
+  // Update the countdown every 100 milliseconds to include milliseconds in the display
+  const timerInterval = setInterval(updateCountdown, 100);
+});
